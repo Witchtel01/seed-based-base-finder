d9a9065170a1cdd0e53e74a37e1b38c080724ab9
Witchtel <engineeringkerbals@gmail.com>
2025-01-03 13:12:55 -0500

    Initial Commit

---
+++ src/main/java/org/witchtel/seedBased_base_finder/SeedBased_base_finder.java
@@ -0,0 +1,10 @@
+package org.witchtel.seedBased_base_finder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SeedBased_base_finder implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/witchtel/seedBased_base_finder/client/SeedBased_base_finderClient.java
@@ -0,0 +1,10 @@
+package org.witchtel.seedBased_base_finder.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SeedBased_base_finderClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
